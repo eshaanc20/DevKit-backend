5d7d4851e9f000feff992eab80df670bc71e963b
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-08-23 23:29:18 -0400

    Fixed error with deleting requests for list

--- routes/delete.js
+++ routes/delete.js
@@ -6,7 +6,12 @@ router.post('/', function(req, res, next) {
     var requestFileData = fs.readFileSync('request.json');
     var requestFile = requestFileData.toString();
     var requests = JSON.parse(requestFile)
-    requests.splice(req.body.id-1,1)
+    var requestIndex = 0;
+    var element = requests.filter((request,index) => {
+        request.id == req.body.id? requestIndex = index: null;
+        return request.id == req.body.id
+    })[0]
+    requests.splice(requestIndex,1)
     var JSONRequestList = JSON.stringify(requests)
     fs.writeFileSync('request.json', JSONRequestList)
     res.send('API not added to list');
